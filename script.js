af12ffea633d1b85c28dbdd7c6779b12ce95c098
rokstarss <liamshorrors@outlook.com>
2025-01-20 20:37:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ document.getElementById("confirmLove").addEventListener("click", function() {
     }
 });
 
+document.getElementById("showActivity").addEventListener("click", function(){
+    const activity = document.getElementById("activity").ariaValueMax;
+    const activityMessage = document.getElementById("activityMessage");
+
+    const messages = {
+        dinner: "Lets get a nummy dinner somewhere :)",
+        movie: "How about a nice little movie :drools everywhere:",
+        bowling: "how about some bowling, i might beat u tho :P",
+    };
+
+    activityMessage.textContent = messages[activity];
+    activityMessage.classList("hidden");
+});
+
+const music = document.getElementById("backgroundMusic");
+const musicButton = document.getElementById("toggleMusic");
+
+musicButton.addEventListener("click", function() {
+    if (music.paused) {
+        music.play();
+        musicButton.textContent = "Pause Music";
+    } else {
+        music.pause();
+        musicButton.textContent = "Play Music";
+    }
+});
